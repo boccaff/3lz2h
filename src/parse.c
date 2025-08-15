eea4c14841912033229f911e8901ab6e2cee4295
boccaff <boccaff@gmail.com>
2025-08-15 00:01:35 +0000

    changed calloc checks agains NULL

--- src/parse.c
+++ src/parse.c
@@ -99,7 +99,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 	}
 
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-	if (header == -1) {
+	if (header == NULL) {
 		printf("Malloc failed to create dbheader\n");
 		return STATUS_ERROR;
 	}
@@ -142,7 +142,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 
 int create_db_header(struct dbheader_t **headerOut) {
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-	if (header == -1) {
+	if (header == NULL) {
 		printf("Malloc failed to create dbheader\n");
 		return STATUS_ERROR;
 	}
